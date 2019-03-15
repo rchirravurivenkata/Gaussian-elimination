8a5e837cc5c0d9af35c5f6c9d985ec91a2ee5811
RC Venkata <48605643+rcvenkata@users.noreply.github.com>
2019-03-15 16:57:21 -0500

    Add files via upload

---
+++ Gaussian-elm.R
@@ -0,0 +1,109 @@
+######################################################################################################################################
+#
+# Gaussian Elimination R code
+#   by Ramakanth Chirravuri Venkata
+# 
+#######################################################################################################################################
+
+## The below function needs an input as numbers or as matrix. 
+## If provided as numbers, please specify total number of columns using total_columns argument, and if the numbers are intended to be rowwise, please use the ByRow argument.
+## If needed, you can flag the partial pivoting and row scaling using the optional arguments.
+
+Gaussian_elm <- function(num_or_matrix,total_columns = 0, ByRow= F, rowScaling = FALSE, partialPivoting= FALSE){
+  if(total_columns != 0){
+    len = length(c(num_or_matrix))
+    mat_left <- mat[nrow(mat), nrow(mat)]
+    mat<- matrix(c(num_or_matrix), ncol = total_columns , byrow = ByRow)}
+  if(total_columns == 0){
+    mat <- num_or_matrix
+    if(ByRow == T){
+      mat <- t(mat)
+    }}
+  
+  ## Pivot identification 
+  
+  for (i in 1:nrow(mat)){
+    pos <- mat[i, i]
+    
+    j <- i+1 
+    idx <- diag(mat)
+    if((nrow(mat) -  i) >= 1){  
+      if(partialPivoting == TRUE) {
+        new_piv <- which(mat[, i] == max(mat[i:nrow(mat), i]))
+        if(length(new_piv) > 1){
+          new_piv <- new_piv[1]}
+        temp <- mat[i, ]
+        mat[i, ] <- mat[new_piv, ]
+        mat[new_piv,] <- temp
+        
+        if(rowScaling == TRUE){
+          mat[i, ] <- mat[i, ]/mat[i, i]}
+        idx <- diag(mat)}
+      
+      else if (idx[i] == 0){
+        zeropivot <- mat[i, ]
+        mat[i, ] <- mat[j, ]
+        mat[j, ] <- zeropivot
+        idx <- diag(mat)
+        
+        if(rowScaling == TRUE){
+          mat[i, ] <- mat[i, ]/mat[i,i]}
+      }
+      
+      
+      ## Performs row operations on the rows below the pivot (of a particular column)
+      
+      for (k in j:nrow(mat)){
+        if(mat[k,i] == 0){
+          mat[k, ] <- mat[k, ]}
+        else{ facter <- mat[k, i]/idx[i]
+        mat[k, ] <- mat[k, ] - (facter *(mat[i,]))
+        }}}}
+  mat1 <- mat
+  print(noquote(" **********************************The Result ****************************************************   "))
+  print(noquote("                                                                                         "))
+  for(i in nrow(mat):1){
+    idx <- diag(mat)
+    if(mat[i, i] == 0){
+      print(noquote(" *********************************************************************************************                                                    "))
+      print(noquote("                                                                                         "))
+      print(paste("Value of the variable x", i, " has no unique solution", sep =""))
+      print(noquote("                                                                                         "))
+    }
+    
+    ## Calculates and prints the value of the variables
+    
+    else{
+      b <- (mat[i, ncol(mat)]-sum(mat[i, -c(i, ncol(mat))]))/mat[i,i]}
+    if(i > 1){
+      mat[1:(i-1), i] <- b*  mat[1:(i-1), i]}
+    print(noquote("                                                                                         "))
+    print(paste("Value of the variable x", i,": ", b, sep = ""))
+    print(noquote("                                                                                         "))
+    print(noquote(" ************************************************************************************************                                                                    "))
+  } 
+  print(noquote("                                                                                         "))
+  print(noquote("               *********** The matrix is returned *************                                              "))
+  print(noquote("                                                                                         "))
+  print(noquote("                                                                                         "))
+  return(mat1) }
+
+
+
+########################################################################################################################################
+
+## For generating the ill-condition matrix (uses the suggestions provided in the class)
+## Incorporates the suggestion of parallelism
+
+
+# mato is for calculating the kappa of random matrices
+
+mato <- 0
+l <- 0
+while(mato < 100000){
+  a <- matrix(runif(110,0.00000000001,1), ncol =11)
+  mato <- kappa(a)
+  l <- a
+}
+
+########################################################################################################################################
---
+++ README.md
@@ -0,0 +1,32 @@
+Gaussian_elm function 
+
+Mandatory arguments:
+
+num_or_matrix – The function can either take in numbers or a matrix.
+
+If the input is numbers: 
+total_columns - (By default its 0) must specify the total number of the columns (including the right hand side). 
+DO NOT use total columns argument if matrix is your input.
+
+Note: Total column argument requires total number of columns including the right hand side. The function recognizes the last column as the right hand side. 
+
+Optional arguments:
+
+ByRow – (By default its FALSE):  If the input values (numbers or matrix) are needed to be considered rowwise (or to be transposed). 
+
+rowScaling – (By default its FALSE): If rowscaling is required in addition to Gaussian elimination.
+
+partialPivoting - (By default its FALSE): If partial pivoting is required in addition to Gaussian elimination.
+
+
+Directions:
+
+The input must either be in the form of a numbers or matrix. The right hand side values must also be provided along with left hand side. The function recognizes the last column of input values to be right hand side. ONLY if vector is an input, please specify the total number of columns (total columns including the right hand side). 
+
+Potential results:
+
+The function prints the partial pivot matrix, if the partial pivoting is TRUE. 
+It prints step-wise Gaussian elimination results.
+In the final stage it prints, the individual variable values.
+The upper triangular (or the final result) is returned by the function.
+
